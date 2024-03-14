d7f9bbe6061db2afa64bb1f7da4e3c8fc3f50e7c
badr <badr@test.com>
2024-03-14 11:10:49 +0000

    test

---
+++ resources/views/layouts/index.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+    @vite(['resources/js/app.js'])
+    @vite(['resources/js/app.js'])
+    @vite('resources/css/app.css')
+</head>
+<body>
+    @yield('content')
+    
+</body>
+</html>
